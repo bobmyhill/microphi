6d1bbb3079a5bcda62155eb35f3b1101d1169bbc
Bob Myhill <myhill.bob@gmail.com>
2019-11-09 13:13:35 +0000

    rename files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # microphi
-Microphi is an open-source python project which provides functions to transform solid solutions from one basis to another. It can either be used to convert between different endmember sets, or to compute macroscopic endmember and interaction energies from microscopic site properties and interactions.
+Microphi is an open-source python miniproject which provides functions to transform solid solutions from one basis to another. It can either be used to convert between different endmember sets, or to compute macroscopic endmember and interaction energies from microscopic site properties and interactions.
 
-The motivation for this project is a paper from Roger Powell and coauthors (2014; https://onlinelibrary.wiley.com/doi/full/10.1111/jmg.12070).
+The motivation for this miniproject is a paper from Roger Powell and coauthors (2014; https://onlinelibrary.wiley.com/doi/full/10.1111/jmg.12070).
+
+The functions and class are stored in a single file, microphi.py. These functions allow the user to read in input files, or declare the desired inputs as python objects. A set of examples are given.
--- asymmetric_formalism/example_two_site_order_disorder.py
+++ asymmetric_formalism/example_two_site_order_disorder.py
@@ -1,30 +1,27 @@
-
 import numpy as np
 from microphi import AsymmetricMicrophiSolution
 
+#  The example in this file corresponds to the example illustrated in Figure 3
+#  of Myhill and Connolly (submitted to GCA).
+
+#  It consists of a two-site model, where both sites are occupied by either
+#  species A or B.
+
+sites = ['1', '1', '2', '2']
+site_species = ['A', 'B', 'A', 'B']
 
 mbrs = ['AA', 'BB', 'AB']
-sites = ['M1', 'M1', 'M2', 'M2']
-site_species = ['A', 'B', 'A', 'B']
 endmember_site_occupancies = np.array([[1., 0., 1., 0.],
-                              [0., 1., 0., 1.],
-                              [1., 0., 0., 1.]])
+                                       [0., 1., 0., 1.],
+                                       [1., 0., 0., 1.]])
+
+# Symmetric solution
 alphas = np.array([1., 1., 1., 1.])
+
+print('Solution 1: Ordered endmembers destabilised, both sites identical')
 Wa = np.array([[0., 1., 0., 0.],
-      [0., 0., 0., -(-4.)], # w(AABB,M1M2) = -4, so 2AB is more stable than AA+BB at 0 K.
-      [0., 0., 0., 1.],
-      [0., 0., 0., 0.]])
-
-ss = AsymmetricMicrophiSolution(mbrs, sites, site_species,
-                                endmember_site_occupancies,
-                                alphas,
-                                site_species_interactions=Wa)
-
-print(ss)
-
-Wa = np.array([[0., 1., 0., 0.],
-               [0., 0., 0., -(-4.)], # w(AABB,M1M2) = -4, so 2AB is more stable than AA+BB at 0 K.
-               [0., 0., 1., 1.],
+               [0., 0., 0., -(8.)], # w(AABB,M1M2) = 8., so 2AB is less stable than AA+BB at 0 K.
+               [0., 0., 0., 1.],
                [0., 0., 0., 0.]])
 
 ss = AsymmetricMicrophiSolution(mbrs, sites, site_species,
@@ -34,12 +31,26 @@ ss = AsymmetricMicrophiSolution(mbrs, sites, site_species,
 
 print(ss)
 
-exit()
 
+print('Solution 2: Ordered endmembers stabilised, both sites identical')
 Wa = np.array([[0., 1., 0., 0.],
-      [0., 0., 0., -(8.)], # w(AABB,M1M2) = 8., so 2AB is less stable than AA+BB at 0 K.
-      [0., 0., 0., 1.],
-      [0., 0., 0., 0.]])
+               [0., 0., 0., -(-4.)], # w(AABB,M1M2) = -4, so 2AB is more stable than AA+BB at 0 K.
+               [0., 0., 0., 1.],
+               [0., 0., 0., 0.]])
+
+ss = AsymmetricMicrophiSolution(mbrs, sites, site_species,
+                                endmember_site_occupancies,
+                                alphas,
+                                site_species_interactions=Wa)
+
+print(ss)
+
+
+print('Solution 3: Ordered endmembers stabilised, A on Site 1 assigned a higher energy than on Site 2')
+Wa = np.array([[1., 1., 0., 0.],
+               [0., 0., 0., -(-4.)], # w(AABB,M1M2) = -4, so 2AB is more stable than AA+BB at 0 K.
+               [0., 0., 0., 1.],
+               [0., 0., 0., 0.]])
 
 ss = AsymmetricMicrophiSolution(mbrs, sites, site_species,
                                 endmember_site_occupancies,
